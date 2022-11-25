cbe67e971843420f70e7b8b9ad427f0a5ec914e7
Adnan Mohamed Z <adnan4d@gmail.com>
2022-11-26 04:58:04 +0530

    Made project a Multi-Module Project

---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/model/Owner.java
@@ -0,0 +1,7 @@
+package com.example.petclinicspring.model;
+
+public class Owner extends Person{
+    public Owner(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/model/PetType.java
@@ -0,0 +1,4 @@
+package com.example.petclinicspring.model;
+
+public class PetType {
+}
