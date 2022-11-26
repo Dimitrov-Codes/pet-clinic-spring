966749b764141bcd6453dbd832c2c6243a3a4e43
Adnan Mohamed Z <adnan4d@gmail.com>
2022-11-26 14:05:26 +0530

    Added Index Page and controller

---
+++ pet-clinic-web/src/main/java/com/example/petclinicspring/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.petclinicspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String getIndex(){
+        return "index";
+    }
+
+
+}
--- pet-clinic-web/src/main/java/com/example/petclinicspring/PetClinicSpringApplication.java
+++ pet-clinic-web/src/main/java/com/example/petclinicspring/PetClinicSpringApplication.java
@@ -2,6 +2,8 @@ package com.example.petclinicspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class PetClinicSpringApplication {
@@ -9,5 +11,9 @@ public class PetClinicSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(PetClinicSpringApplication.class, args);
     }
+    @RequestMapping("/error")
+    public String getError(Model model) {
 
+        return "error";
+    }
 }
