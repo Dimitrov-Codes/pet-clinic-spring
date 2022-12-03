18280a1d95af507a74a0c1a32f59c91572cc439a
Adnan Mohamed Z <adnan4d@gmail.com>
2022-12-03 21:50:32 +0530

    Added PetTypeMapService

---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.example.petclinicspring.services;
+
+import com.example.petclinicspring.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType>{
+}
---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/services/map/PetTypeMapService.java
@@ -0,0 +1,9 @@
+package com.example.petclinicspring.services.map;
+
+import com.example.petclinicspring.model.PetType;
+import com.example.petclinicspring.services.PetService;
+import com.example.petclinicspring.services.PetTypeService;
+
+public class PetTypeMapService extends AbstractMapService<PetType> implements PetTypeService {
+
+}
