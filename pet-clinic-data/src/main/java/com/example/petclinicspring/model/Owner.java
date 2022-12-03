15a0936077c351d7093374258e8a1f6132a0705e
Adnan Mohamed Z <adnan4d@gmail.com>
2022-12-03 21:45:43 +0530

    Added Contact details for Owner

--- pet-clinic-data/src/main/java/com/example/petclinicspring/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/model/Owner.java
@@ -4,10 +4,37 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String phone;
     private Set<Pet> pets = new HashSet<>();
 
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public Set<Pet> getPets() {
