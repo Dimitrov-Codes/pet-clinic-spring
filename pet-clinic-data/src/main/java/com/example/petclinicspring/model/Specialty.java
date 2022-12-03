10d8007baea65f34fc5d44ae9bcf75a353c1e01c
Adnan Mohamed Z <adnan4d@gmail.com>
2022-12-03 21:43:45 +0530

    Added Specialty and associated Vet with it

---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/model/Specialty.java
@@ -0,0 +1,14 @@
+package com.example.petclinicspring.model;
+
+
+public class Specialty extends BaseEntity {
+    private String desc;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
--- pet-clinic-data/src/main/java/com/example/petclinicspring/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/model/Vet.java
@@ -1,6 +1,18 @@
 package com.example.petclinicspring.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
     }
